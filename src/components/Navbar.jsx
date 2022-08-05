1859738e25c14e772a1c8964fa54914e95c310e4
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-08-05 13:45:26 -0500

    navbar adjustment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FaBars, FaTimes} from 'react-icons/fa';
 import Logo from '../assets/logo.jpeg';
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
@@ -10,32 +13,30 @@ const Navbar = () => {
         </div>
 
         {/*Menu*/}
-        <div>
-            <ul className='flex'>
+            <ul className='hidden md:flex'>
                 <li>Home</li>
                 <li>About</li>
                 <li>Skills</li>
                 <li>Work</li>
                 <li>Contact</li>
             </ul>
-        </div>
 
         {/*Hamburger*/}
-        <div className='hidden'>
+        <div onClick={handleClick} className='md:hidden z-10'>
             <FaBars />
         </div>
 
         {/*Mobile Menu*/}
-        <ul className='hidden'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Work</li>
-            <li>Contact</li>
+        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+            <li className='py-6 text-4xl'>Home</li>
+            <li className='py-6 text-4xl'>About</li>
+            <li className='py-6 text-4xl'>Skills</li>
+            <li className='py-6 text-4xl'>Work</li>
+            <li className='py-6 text-4xl'>Contact</li>
         </ul>
 
         {/*Social Icons*/}
-        <div>
+        <div className='hidden'>
 
         </div>
     </div>
