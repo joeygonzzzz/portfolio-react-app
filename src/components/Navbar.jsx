5485dad9cfdb2d1d79a047616a676b591229f9d2
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-08-05 10:47:25 -0500

    hamburger and list items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa';
+import Logo from '../assets/logo.jpeg';
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div>
+            <img src={Logo} alt="Logo Image" style={{width: '50px'}} />
+        </div>
 
+        {/*Menu*/}
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+
+        {/*Hamburger*/}
+        <div className='hidden'>
+            <FaBars />
+        </div>
+
+        {/*Mobile Menu*/}
+        <ul className='hidden'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact</li>
+        </ul>
+
+        {/*Social Icons*/}
+        <div>
+
+        </div>
     </div>
   )
 }
