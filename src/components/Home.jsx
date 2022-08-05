d22678cb0cd973430873a617d7927e21122a38da
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-08-05 14:31:53 -0500

    fix hover

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
           <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a software Engineer</h2>
           <p className="text-[#8892b0] py-4 max-w-[700px]">Web developer with a focus on the front-end, utilizing JavaScript ES6, communications, and various team building methodologies. Prior background includes 7+ years of education, continuously seeking ways to simplify ideas and instruction. Seeking an exciting opportunity to utilize my combined experience while bringing fresh ideas to fruition. </p>
           <div>
-            <button className="text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-blue-600 hover:border-white">View Work
-            <span className="hover:rotate-90 duration-300">
+            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-blue-600 hover:border-white">View Work
+            <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3"/>
             </span>
             </button>
