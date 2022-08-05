db452ba083b5220b160170710fce02eaf4dd120a
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-08-05 15:34:59 -0500

    adjustment

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,11 +10,11 @@ import Github from '../assets/github.png';
 
 const Skills = () => {
   return (
-    <div name="skills" className="bg-[#0a192f] text-gray-300 ">
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300 ">
         {/*Container*/}
         <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
             <div>
-                <p className="text-4xl font-bold inline border-b-4 border-white">Experience</p>
+                <p className="text-4xl font-bold inline border-b-4 border-white">Skills</p>
                 <p className="py-4">These are the languages that I've worked with</p>
             </div>
 
