ce08bab31d976924f36211753a12824d1ac08f91
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-08-08 22:04:17 -0500

    Added more projects and images, will keep six

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import LvBand from '../assets/lvband.png';
 import rosaStand from '../assets/rosastand.png';
 import mazeGame from '../assets/mazegame.png';
+import movieFight from '../assets/moviefight.png';
+import secretMessage from '../assets/secretmessage.png';
+import portfolioSite from '../assets/portfoliosite.png';
 
 const Work = () => {
   return (
@@ -13,7 +16,6 @@ const Work = () => {
             </div>
 
             {/*Container*/}
-
             <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
 
                 {/*Grid Item*/}
@@ -35,6 +37,7 @@ const Work = () => {
                         </div>
                     </div>
                 </div>
+
                 <div style={{backgroundImage: `url(${rosaStand})`}}
                 className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
                     
@@ -72,10 +75,72 @@ const Work = () => {
                         </div>
                     </div>
                 </div>
-            </div>
 
+                {/*New Projects, to be replaced with newer ones*/}
+                <div style={{backgroundImage: `url(${movieFight})`}}
+                className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+                    
+                    {/*Hover Effect*/}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            JS, API For Data
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="https://movie-project-five.vercel.app/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="https://github.com/joeygonzzzz/Movie-Project">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                
+                <div style={{backgroundImage: `url(${secretMessage})`}}
+                className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+                    
+                    {/*Hover Effect*/}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            JS, Base64 Encoding
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="https://secret-message-peach-ten.vercel.app/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="https://github.com/joeygonzzzz/secret-message">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div style={{backgroundImage: `url(${portfolioSite})`}}
+                className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+                    
+                    {/*Hover Effect*/}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            React, Tailwind, GetForm.io
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href="https://portfolio-react-app-eight.vercel.app/">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href="https://github.com/joeygonzzzz/portfolio-react-app">
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+                </div>
+            </div>
         </div>
-    </div>
   )
 }
 
